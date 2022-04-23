adcbefffd6600510bcba374bff1ead34fc2bb914
core.pager=cat <ericonstantngatia@gmail.com>
2022-04-23 23:00:04 +0000

    first

---
+++ index.sql
@@ -0,0 +1,92 @@
+        INSERT INTO Costumer
+        (
+            Costumer_Id,
+            Costumer_Name,
+            Costumer_Tel,
+        );
+        VALUES
+        (
+            'C01',
+            'ALI',
+            773221009,
+        );
+
+        (
+            Costumer_Id,
+            Costumer_Name,
+            Costumer_Tel,
+        )
+        VALUES
+        (
+            'C02',
+            'ASMA',
+            77345823,
+        );
+
+INSERT INTO Product
+    (
+        Product_Id,
+        Product_Name,
+        Quantity ,
+        Price ,
+    )
+
+    (
+        'P01',
+        'SAMSUNG GALAXY S20',
+        'SMATPHONE',
+        3299
+    );
+
+    (
+        Product_Id,
+        Product_Name,
+        Quantity ,
+        Price ,
+    )
+
+    (
+        'P02',
+        'ASUS Notebook',
+        'PC',
+        4599
+    );
+
+
+
+
+
+
+    INSERT INTO Orders
+    (
+        Costumer_Id,
+        Product,
+        OrderDate,
+        Quantity,
+        Totam_amount,
+    )
+
+    (
+        'C01',
+        'P02',
+        'NULL',
+        2,
+        9198,
+    );
+
+
+(
+        Costumer_Id,
+        Product,
+        OrderDate,
+        Quantity,
+        Totam_amount,
+    )
+
+    (
+        'C02',
+        'P01',
+        '28/05/2020',
+        1,
+        3299,
+    );
